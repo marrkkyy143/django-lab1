eff32105caa3ba962bf030e986727d741063e1b1
Mariia <mariavatrushchak143@gmail.com>
2026-09-21 13:02:16 +0300

    Added blog app and database setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [ #список встановлених Django-додаткі�
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog',
 ]
 
 MIDDLEWARE = [
